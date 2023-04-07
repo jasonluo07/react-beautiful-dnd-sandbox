f27ca84bb953e4a82f30ba129f9b2bb8f91b8622
jasonluo <yulaie1012@gmail.com>
2023-04-07 23:37:41 +0800

    feat: render columns and tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+import initialData from './initial-dats';
+import Column from './components/Column';
+
 function App() {
+  const [data, setData] = useState(initialData);
+
   return (
     <div>
-      <h1>Hello, React Beautiful DnD!</h1>
+      {data.columnOrder.map(columnId => {
+        const column = data.columns[columnId];
+        const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
     </div>
   );
 }
