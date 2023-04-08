56329e17b52aa3f8f50efc94577c1c8d063235c1
jasonluo <yulaie1012@gmail.com>
2023-04-08 14:22:36 +0800

    feat: add <DragDropContext />

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import { useState } from 'react';
 import initialData from './initialData';
 import Column from './components/Column';
 import { IData } from './types';
+import { DragDropContext } from '@hello-pangea/dnd';
 
 export default function App() {
   const [data, setData] = useState<IData>(initialData);
 
+  function handleDragEnd() {
+    // TODO: update state
+  }
+
   return (
-    <div>
+    <DragDropContext onDragEnd={handleDragEnd}>
       {data.columnOrder.map(columnId => {
         const column = data.columns[columnId];
         const tasks = column.taskIds.map(taskId => data.tasks[taskId]);
         return <Column key={column.id} column={column} tasks={tasks} />;
       })}
-    </div>
+    </DragDropContext>
   );
 }
