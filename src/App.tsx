66a908aaf8ecc917fc4f0e0c2b8b1df3ae83928d
jasonluo <yulaie1012@gmail.com>
2023-04-08 15:47:03 +0800

    feat: add DropResult login

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,30 @@ import { useState } from 'react';
 import initialData from './initialData';
 import Column from './components/Column';
 import { IData } from './types';
-import { DragDropContext } from '@hello-pangea/dnd';
+import { DragDropContext, DropResult } from '@hello-pangea/dnd';
+import produce from 'immer';
 
 export default function App() {
   const [data, setData] = useState<IData>(initialData);
 
-  function handleDragEnd() {
-    // TODO: reordering logic
+  function handleDragEnd(result: DropResult) {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return;
+    }
+
+    setData(prevData => {
+      return produce(prevData, draft => {
+        const column = draft.columns[source.droppableId];
+        column.taskIds.splice(source.index, 1);
+        column.taskIds.splice(destination.index, 0, draggableId);
+      });
+    });
   }
 
   return (
