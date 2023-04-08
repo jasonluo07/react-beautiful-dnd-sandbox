cf1a3e8248df130cbd2c00e701ea348514702cf6
jasonluo <yulaie1012@gmail.com>
2023-04-08 14:38:17 +0800

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
   const [data, setData] = useState<IData>(initialData);
 
   function handleDragEnd() {
-    // TODO: update state
+    // TODO: reordering logic
   }
 
   return (
