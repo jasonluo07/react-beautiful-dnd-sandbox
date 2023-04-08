33c15de25aa9e542e1132512addc2344a849c1ca
jasonluo <yulaie1012@gmail.com>
2023-04-08 21:13:44 +0800

    feat: 使用 isDragDisabled 禁止特定 <Draggable /> 拖動

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,12 +2,17 @@ import styled from 'styled-components';
 import { ITask } from '../types';
 import { Draggable } from '@hello-pangea/dnd';
 
-const Container = styled.div<{ isDragging: boolean }>`
+const Container = styled.div<{ isDragging: boolean; isDragDisabled: boolean }>`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+  background-color: ${props =>
+    props.isDragDisabled
+      ? 'lightgrey'
+      : props.isDragging
+        ? 'lightgreen'
+        : 'white'};
 `;
 
 interface ITaskProps {
@@ -16,14 +21,21 @@ interface ITaskProps {
 }
 
 export default function Task(props: ITaskProps) {
+  const isDragDisabled = ['task-1', 'task-2'].includes(props.task.id);
+
   return (
-    <Draggable draggableId={props.task.id} index={props.index}>
+    <Draggable
+      draggableId={props.task.id}
+      index={props.index}
+      isDragDisabled={isDragDisabled}
+    >
       {(provided, snapshot) => (
         <Container
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           isDragging={snapshot.isDragging}
+          isDragDisabled={isDragDisabled}
         >
           {props.task.content}
         </Container>
